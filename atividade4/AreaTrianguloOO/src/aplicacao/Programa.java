50e07aa80085a3e630c4bd40c166038a657ba9f1
Mumu <gabrielromao1818@gmail.com>
2026-05-15 22:12:22 -0300

    .

--- atividade4/AreaTrianguloOO/src/aplicacao/Programa.java
+++ atividade4/AreaTrianguloOO/src/aplicacao/Programa.java
@@ -13,7 +13,8 @@ public class Programa {
 
         Triangulo trianguloX, trianguloY; // Declara as referências
         trianguloX = new Triangulo();     // Instancia o objeto trianguloX
-        trianguloY = new Triangulo();     // Instancia o objeto trianguloY
+        trianguloY = new Triangulo();
+        Triangulo trianguloZ = new Triangulo();     // Instancia o objeto trianguloY
 
         System.out.println("Digite as medidas do triângulo X:");
         trianguloX.a = sc.nextDouble();
@@ -24,6 +25,11 @@ public class Programa {
         trianguloY.a = sc.nextDouble();
         trianguloY.b = sc.nextDouble();
         trianguloY.c = sc.nextDouble();
+        
+        System.out.println("Digite as medidas do triângulo Z:");
+        trianguloZ.a = sc.nextDouble();
+        trianguloZ.b = sc.nextDouble();
+        trianguloZ.c = sc.nextDouble();
 
         double p = (trianguloX.a + trianguloX.b + trianguloX.c) / 2.0;
         double areaX = Math.sqrt(p * (p - trianguloX.a) * (p - trianguloX.b) * (p - trianguloX.c));
@@ -31,14 +37,22 @@ public class Programa {
         p = (trianguloY.a + trianguloY.b + trianguloY.c) / 2.0;
         double areaY = Math.sqrt(p * (p - trianguloY.a) * (p - trianguloY.b) * (p - trianguloY.c));
 
+        p = (trianguloZ.a + trianguloZ.b + trianguloZ.c) / 2.0;
+        double areaZ = Math.sqrt(p * (p - trianguloZ.a) * (p - trianguloZ.b) * (p - trianguloZ.c));
+
         System.out.printf("Área do triângulo X: %.4f%n", areaX);
         System.out.printf("Área do triângulo Y: %.4f%n", areaY);
+        System.out.printf("Área do triângulo Z: %.4f%n", areaZ);
 
-        if (areaX > areaY) {
+        if (areaX > areaY && areaX > areaZ) {
             System.out.println("Maior área: X");
-        } else {
+        } else if (areaY > areaZ){
             System.out.println("Maior área: Y");
+        } else{
+            System.out.println("Maior área: Z");
         }
+        
+
         sc.close();
     }
 }
